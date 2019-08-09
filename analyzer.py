507c77752b91bfa00d7043ff72f79e03e955659e
Jacob Lucas <lucas.jacob.robert@gmail.com>
2019-08-08 22:04:06 -0400

    fully working

--- analyzer.py
+++ analyzer.py
@@ -15,38 +15,38 @@ def tracker(quotes):
         with Database() as db:
             db.cursor.execute('''SELECT price FROM log WHERE ticker="{}";
             '''.format('TVIX'))
-            latest_trade_price = db.cursor.fetchall()[-1][0]
-            no_trades = len(db.cursor.fetchall())
-        latest_change = quotes['tvix_current'] - latest_trade_price
+            all_trades = db.cursor.fetchall()
+        try:
+            latest_trade_price = all_trades[-1][0]
+            no_trades = len(all_trades)
+            latest_change = quotes['tvix_current'] - latest_trade_price
+        except:
+            latest_trade_price = 0
+            latest_change = 0
 
         with Database() as db:
             db.cursor.execute('''SELECT stop_loss FROM log WHERE ticker="{}";
             '''.format('TVIX'))
-        current_stop_loss = db.cursor.fetchall()[-1][0]
+        try:
+            current_stop_loss = db.cursor.fetchall()[-1][0]
+        except:
+            current_stop_loss = 0
 
-        if current_stop_loss <= quotes['tvix_current']:
+        if current_stop_loss >= quotes['TVIX_current']:
             pass
             ## TODO: sell all
 
         elif latest_change >= 0.8:
             # TODO: lever up
-            pass
+            stop_loss = quotes['svxy_current'] - 0.50
+            return Trader.tvix_buy_trade_log('TVIX',quotes['tvix_open'],
+            quotes['tvix_current'],stop_loss)
 
         else:
             # initial trade at 100 shares
             stop_loss = quotes['tvix_current'] - 0.50
-            return Trader.tvix_trade_log('TVIX',quotes['tvix_open'],quotes['tvix_current'],stop_loss)
-
-
-
-
-
-
-
-
-
-
-
+            return Trader.tvix_buy_trade_log('TVIX',quotes['tvix_open'],
+            quotes['tvix_current'],stop_loss)
 
     elif tvix_change < 0.3 and svxy_change > 0.3:
         with Database() as db:
@@ -60,6 +60,7 @@ def tracker(quotes):
         except:
             latest_trade_price = 0
             latest_change = 0
+            no_trades = 0
 
         with Database() as db:
             db.cursor.execute('''SELECT stop_loss FROM log WHERE ticker="{}";
@@ -70,18 +71,21 @@ def tracker(quotes):
             current_stop_loss = 0
 
         if current_stop_loss >= quotes['svxy_current']:
-            pass
-            ## TODO: sell all
+            sell_price = quotes['svxy_current']
+            return trader.svxy_sell_gains(sell_price)
 
         elif latest_change >= 0.8:
             # TODO: lever up
             stop_loss = quotes['svxy_current'] - 0.50
-            return Trader.svxy_trade_log('SVXY',quotes['svxy_open'],quotes['svxy_current'],stop_loss)
+            return Trader.svxy_buy_trade_log('SVXY',quotes['svxy_open'],
+            quotes['svxy_current'],stop_loss)
 
-        else:
+        elif no_trades == 0:
             # initial trade at 100 shares
             stop_loss = quotes['svxy_current'] - 0.50
-            return Trader.svxy_trade_log('SVXY',quotes['svxy_open'],quotes['svxy_current'],stop_loss)
-
+            return Trader.svxy_buy_trade_log('SVXY',quotes['svxy_open'],
+            quotes['svxy_current'],stop_loss)
+        else:
+            print('no trades for svxy')
     else:
         print('no dice')
--- trader.py
+++ trader.py
@@ -16,7 +16,7 @@ class Trader:
     def __exit__(self):
         pass
 
-    def tvix_trade_log(ticker, open_price, current_price, stop_loss):
+    def tvix_buy_trade_log(ticker, open_price, current_price, stop_loss):
         with Database() as db:
             db.cursor.execute(
                 '''
@@ -27,12 +27,31 @@ class Trader:
             )
         print('bought')
 
-    def svxy_trade_log(ticker, open_price, current_price, stop_loss):
+    def svxy_buy_trade_log(ticker, open_price, current_price, stop_loss):
         with Database() as db:
             db.cursor.execute(
-                '''INSERT INTO log(time,ticker,price,stop_loss,volume,buy_sell)
+                '''
+                    INSERT INTO log(time,ticker,price,stop_loss,volume,buy_sell)
                     VALUES(?,?,?,?,?,?);
                 ''',
                     (tm,ticker,current_price,stop_loss,100,'buy')
             )
         print('bought')
+
+    def tvix_sell_gains(sell_price):
+        pass
+
+    def svxy_sell_gains(sell_price):
+        with Database() as db:
+            db.cursor.execute(
+            '''SELECT * FROM log WHERE ticker="SVXY";'''
+            )
+            all_trades = db.cursor.fetchall()
+
+        gain_dict = {}
+        overall_gains = 0
+        for i in all_trades:
+            gain = i[3] - sell_price
+            gain_dict[i[1]] = gain
+
+        print(gain_dict)
